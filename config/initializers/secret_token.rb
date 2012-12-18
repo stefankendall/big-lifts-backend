4bfec3908967ea2aa7042dc960df7d4d2b87b192
Stefan Kendall <stefankendall@gmail.com>
2012-12-18 13:01:24 -0500

    Starting projects

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BigLiftsBackend::Application.config.secret_token = 'ac5764b8f8e8be709a969ff474a3efc1badfc437de5a9f09b72848653a36bb384db8a03a4a5ae95c65cc9b9d1c27228e88d3fee9a9de490cb276f26f0fdcfb43'
