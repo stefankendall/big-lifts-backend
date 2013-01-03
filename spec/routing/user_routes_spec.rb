432f5930ad6f85a99630be8cee16a6d7074c3daa
Stefan Kendall <stefankendall@gmail.com>
2013-01-03 08:30:17 -0500

    Converted lsat test::unit spec into rspec.

--- spec/routing/user_routes_spec.rb
+++ spec/routing/user_routes_spec.rb
@@ -1,7 +1,7 @@
-require 'test_helper'
+require 'spec_helper'
 
-class RoutesTest < ActionController::TestCase
-  test "creating a user" do
+describe UsersController do
+  it "should recognize the create user path" do
     assert_routing({:path => 'users', :method => :post}, {:controller => 'users', :action => 'create'})
   end
 end
