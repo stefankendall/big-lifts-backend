5beb314b0dfc7565d8bb61cf30cec8ce7e461395
Stefan Kendall <stefankendall@gmail.com>
2013-01-06 12:53:48 -0500

    Fixed log specs

--- spec/controllers/log_controller_spec.rb
+++ spec/controllers/log_controller_spec.rb
@@ -2,33 +2,35 @@ require 'spec_helper'
 
 describe LogController do
   let(:user) { create_user() }
+  let(:auth) { {:username => user.username, :password => user.password} }
+
   describe "POST #create" do
     it "will fail to save logs without an associated workout" do
-      post :create, {:username => user.username, :password => user.password}
+      post :create, auth
       response.status.should == 400
     end
 
     it "will fail to save logs without a specified user" do
-      post :create, {:username => user.username, :password => user.password, :workout_id => '1', :logs => [{sets: 5}]}
+      post :create, {:workout_id => '1', :logs => [{sets: 5}]}
       response.status.should == 401
     end
 
     it "will not save workouts with an empty log set" do
-      post :create, {:username => user.username, :workout_id => '1'}
+      post :create, {:workout_id => '1'}.merge(auth)
       response.status.should == 400
     end
 
     it "will save workouts with an associated log" do
-      post :create, {:username => user.username, :workout_id => '1', :logs => [{sets: 5}]}
+      post :create, {:workout_id => '1', :logs => [{sets: 5}]}.merge(auth)
       response.status.should == 200
     end
   end
 
   describe "PUT #update" do
     it "will update existing logs" do
-      post :create, {:username => user.username, :password => user.password, :workout_id => '1', :logs => [{sets: 5}]}
-      put :update, {:id => '1', :username => user.username, :password => user.password, :workout_id => '1', :logs => [{sets: 6}, {reps: 5}]}
-      get :index, {:username => user.username, :password => user.password}
+      post :create, {:workout_id => '1', :logs => [{sets: 5}]}.merge(auth)
+      put :update, {:id => '1', :workout_id => '1', :logs => [{sets: 6}, {reps: 5}]}.merge(auth)
+      get :index, auth
       ActiveSupport::JSON.decode(response.body).length.should == 1
       ActiveSupport::JSON.decode(response.body)[0]["logs"].length.should == 2
       Log.count().should == 2
@@ -40,7 +42,7 @@ describe LogController do
       logs = [Log.create({:sets => 5, :reps => 3, name: "Press"}), Log.create({:sets => 5, :reps => 3, name: 'Power Clean'})]
       user.workouts() << Workout.create(local_workout_id: '1', logs: logs)
 
-      get :index, {:username => user.username, :password => user.password}
+      get :index, auth
       ActiveSupport::JSON.decode(response.body).length.should == 1
       ActiveSupport::JSON.decode(response.body)[0]["logs"].length.should == 2
     end
