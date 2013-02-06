806ca4ab13f574adfc09c244c0f63cb2af2003ee
Stefan Kendall <stefankendall@gmail.com>
2013-02-06 15:51:22 -0500

    Attempting to migrate production workout_id columb

--- db/migrate/20130206204238_change_workout_id_to_integer.rb
+++ db/migrate/20130206204238_change_workout_id_to_integer.rb
@@ -1,5 +1,13 @@
 class ChangeWorkoutIdToInteger < ActiveRecord::Migration
-  def change
-    change_column :workouts, :workout_id, :integer
+  def up
+    if Rails.env == 'production'
+      connection.execute(%q{alter table workouts alter column workout_id type integer using cast(workout_id as integer)})
+    else
+      change_column :workouts, :workout_id, :integer
+    end
+  end
+
+  def down
+    change_column :workouts, :workout_id, :string
   end
 end
