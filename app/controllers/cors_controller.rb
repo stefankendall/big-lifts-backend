ff82b24e1dc89a4251e5cbac9611218968ac35e8
Stefan Kendall <stefankendall@gmail.com>
2013-03-16 17:58:19 -0400

    Added AppVersion to cors controller

--- app/controllers/cors_controller.rb
+++ app/controllers/cors_controller.rb
@@ -2,7 +2,7 @@ class CorsController < ApplicationController
   def preflight
     headers['Access-Control-Allow-Origin'] = '*'
     headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, Cache-Control, Pragma, Content-Type, Authorization'
+    headers['Access-Control-Allow-Headers'] = 'X-Requested-With, Cache-Control, Pragma, Content-Type, Authorization, AppVersion'
     headers['Access-Control-Max-Age'] = '1728000'
     render nothing: true, :content_type => 'text/plain'
   end
